832e9b440b1795120cc04bb78a6f1d9b21e78cb4
tutyeraa <75582802+tutyeraa@users.noreply.github.com>
2021-03-21 23:21:30 +0600

    Add files via upload

---
+++ ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    /*
+        This function return that all post rows 
+    */
+
+    public function index(Request $request){
+        $posts = Post::all();
+
+        return response($posts, 200);
+
+    }
+
+    /*
+        Return post with post_id
+    */
+    public function get_post(Request $request){
+        $post = Post::find($request->post_id); 
+
+        if($post == null){
+            return response (['message' => 'There is no post'], 404);
+        }
+
+    return response($post, 200);
+    }
+}
